d4c96e374b159ad000dd5f6577ebadb0564976f4
mpmpx <pma@ucdavis.edu>
2020-05-23 05:05:46 -0400

    Update Statistics.java
    Added method for loading a data set into the model, and method for clear the loaded data set.

--- Statistics.java
+++ Statistics.java
@@ -9,7 +9,23 @@ import java.util.HashMap;
  */
 public class Statistics {
 	private double[] arr;
-
+		
+	/**
+	 * Read dataset from an ArrayList.
+	 * @param dataset given dataset.
+	 */
+	public void readData(ArrayList<Double> dataset) {
+		this.arr = new double[dataset.size()];
+		for (int i = 0; i < dataset.size(); i++) {
+			this.arr[i] = dataset.get(i);
+		}
+		
+		Arrays.sort(this.arr);
+	}
+	
+	public void clear() {
+		this.arr = null;
+	}
 	
 	/**
 	 * The main function used for debugging.
