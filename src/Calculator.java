218c256ca62769c1d36a7d2fd1a2472419150f01
weskerliu <weskerliu88@gmail.com>
2020-05-23 14:42:47 -0400

    loadFormUser

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,12 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * A statistics calculator simulator that reads a random number of value from a uniform distribution,
- * and then provides measures for the given data set upon the request from a user. It also supports a
- * command line user interface, a few options for loading and reloading data, and exceptions handling
- * for user's input.
+ * A statistics calculator simulator that reads a random number of value from a
+ * uniform distribution, and then provides measures for the given data set upon
+ * the request from a user. It also supports a command line user interface, a
+ * few options for loading and reloading data, and exceptions handling for
+ * user's input.
  *
  */
 public class Calculator {
@@ -15,9 +14,9 @@ public class Calculator {
 	private Statistics model;
 	private Scanner scanner;
 	private String seperator;
-	
+
 	/**
-	 * Constructor. Initialize the descriptive statistics model, the user input 
+	 * Constructor. Initialize the descriptive statistics model, the user input
 	 * scanner.
 	 */
 	public Calculator() {
@@ -37,14 +36,15 @@ public class Calculator {
 	}
 
 	/**
-	 * Start the calculator. Asking the user for loading a data set first. 
-	 * After successfully loading a data set, move to the main function selection section
-	 * which provides multiple measures for the given data set and a option for reloading
-	 * a new data set. Terminate the execution when the user choose to exit.
+	 * Start the calculator. Asking the user for loading a data set first. After
+	 * successfully loading a data set, move to the main function selection section
+	 * which provides multiple measures for the given data set and a option for
+	 * reloading a new data set. Terminate the execution when the user choose to
+	 * exit.
 	 */
 	public void start() {
 		System.out.println("Hi, welcome to use this statistical calculator.\n");
-		
+
 		// Choose to load data set into the calculator
 		boolean isFinished = false;
 		do {
@@ -55,24 +55,50 @@ public class Calculator {
 			System.out.print("Your choice (1-2): ");
 			String choice = scanner.nextLine();
 			switch (choice) {
-				case "1":
-					loadData();
-					isFinished = true;
-					break;
-				case "2":
-					System.out.println("Good bye.");
-					return;
-				default:
-					System.out.println("Invalid choice, try again.\n");
-					continue;
+			case "1":
+				loadData();
+				isFinished = true;
+				break;
+			case "2":
+				System.out.println("Good bye.");
+				return;
+			default:
+				System.out.println("Invalid choice, try again.\n");
+				continue;
 			}
-			
+
 			isFinished = selectFunction();
 		} while (!isFinished);
 	}
-	
+
+	/**
+	 * Load data set from user's manual input.
+	 * 
+	 * @return true if the process is executed successfully. Otherwise, return
+	 *         false.
+	 */
+	private boolean loadFromUser() {
+		System.out.println(seperator);
+		System.out.println(
+				"Please enter numbers seperated by commas, press enter to ternimate input (1000 numbers at most): ");
+		String input = scanner.nextLine();
+		ArrayList<Double> dataset = new ArrayList<Double>();
+		try {
+			String[] data = input.split(",");
+			for (int i = 0; i < 1000 && i < data.length; i++) {
+				dataset.add(Double.parseDouble(data[i]));
+			}
+			model.readData(dataset);
+		} catch (Exception e) {
+			System.out.println("Invalid input.\n");
+			return false;
+		}
+		return true;
+	}
+
 	/**
 	 * The main method is where the calculator is created, turned on and closed.
+	 * 
 	 * @param args program arguments.
 	 */
 	public static void main(String[] args) {
@@ -80,5 +106,5 @@ public class Calculator {
 		calculator.start();
 		calculator.close();
 	}
-	
+
 }
