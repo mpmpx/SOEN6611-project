d0da3d3770edb678fa026c57944d885005671a56
mpmpx <pma@ucdavis.edu>
2020-05-23 04:56:52 -0400

    Create Statistics.java
    Created Statistics class.

---
+++ Statistics.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Represents a descriptive statistics model which reads a random number of data
+ * and provides measures of the given data set. 
+ *
+ */
+public class Statistics {
+	private double[] arr;
+
+	
+	/**
+	 * The main function used for debugging.
+	 * @param args program arguments
+	 */
+	public static void main(String[] args) {
+
+	}
+	
+}
