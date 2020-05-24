fff1c7e06347f6a17c905e35eeb59451a1536734
Robin Karlose <robin.karlose91@gmail.com>
2020-05-24 18:36:48 -0400

    Added Data Generator class
    Commit for Data Generator class

---
+++ src/DataGenerator.java
@@ -0,0 +1,14 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * A generator that generates random numbers from a uniform distribution.
+ *
+ */
+
+public class DataGenerator
+{
+
+}
