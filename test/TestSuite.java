7185a6b52ebb394a0ec72c56d6833b127dd05d4a
san089 <sanchitkumar013@gmail.com>
2020-05-28 20:57:07 -0400

    Adding test cases

---
+++ test/TestSuite.java
@@ -0,0 +1,19 @@
+/**
+ * <h1>Statistics Test</h1>
+ * This class provides the test suite for this project.
+ * <p>
+ *
+ * @author  Sanchit Kumar
+ * @version 1.0
+ * @since   2020-05-17
+ */
+
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({StatisticsTest.class})
+public class TestSuite {
+    // Test suite for Project
+}
